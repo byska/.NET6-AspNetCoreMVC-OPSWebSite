b4596a08986a63788b153f8b546d272d8ded656c
hüseyin gülerman <huseyingulerman.1997@gmail.com>
2023-10-25 13:35:47 +0300

    .

--- Ops.Web/Program.cs
+++ Ops.Web/Program.cs
@@ -63,7 +63,7 @@ namespace Ops.Web
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
