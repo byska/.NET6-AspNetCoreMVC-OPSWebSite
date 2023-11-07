87bb79553e6d7b283c368b3e5dc5e55bde368601
Beste Yasak <Beyasak@hotmail.com>
2023-11-07 11:12:58 +0300

    .

--- Ops.Repository/Seeds/CategorySeed.cs
+++ Ops.Repository/Seeds/CategorySeed.cs
@@ -13,9 +13,14 @@ namespace Ops.Repository.Seeds
     {
         public void Configure(EntityTypeBuilder<Category> builder)
         {
-            builder.HasData(new Category { Id = 1, Name = "Tişört", Status = Core.Enums.Status.Added },
-                new Category { Id = 2, Name = "Sweat", Status = Core.Enums.Status.Added },
-                new Category { Id = 3, Name = "Çanta", Status = Core.Enums.Status.Added });
+            builder.HasData(new Category { Id = 1, Name = "Tişört Regular", Status = Core.Enums.Status.Added },
+                new Category { Id = 2, Name = "Tişört Oversize", Status = Core.Enums.Status.Added },
+                new Category { Id = 3, Name = "Hoodie", Status = Core.Enums.Status.Added },
+                new Category { Id = 4, Name = "Sweatshirt", Status = Core.Enums.Status.Added },
+                new Category { Id = 5, Name = "Kol Çantası", Status = Core.Enums.Status.Added },
+                new Category { Id = 6, Name = "Cüzdan", Status = Core.Enums.Status.Added },
+                new Category { Id = 7, Name = "Baf", Status = Core.Enums.Status.Added }
+                );
         }
     }
 }
--- Ops.Repository/Seeds/ProductSeed.cs
+++ Ops.Repository/Seeds/ProductSeed.cs
@@ -16,9 +16,12 @@ namespace Ops.Repository.Seeds
         {
             builder.HasData(new Product { Id=1, CategoryId=1, Name="Tişört", Price=200, Status=Status.Added,Amount=200 },
                 new Product { Id = 2, CategoryId=1,  Name = "Kısa Kollu Tişört", Price = 150,  Status = Status.Added,Amount=10 },
-                new Product { Id = 3, CategoryId = 1, Name = " Uzun Kollu Tişört", Price = 300,  Status = Status.Added,Amount=2 },
-                new Product { Id = 4, CategoryId = 3, Name = "Çanta", Price = 400, Status = Status.Added,Amount=20 },
-                new Product { Id = 5, CategoryId = 2, Name = "Sweat", Price = 500,  Status = Status.Added,Amount=100 });
+                new Product { Id = 3, CategoryId = 2, Name = " Uzun Kollu Tişört", Price = 300,  Status = Status.Added,Amount=2 },
+                new Product { Id = 4, CategoryId = 5, Name = "Çanta", Price = 400, Status = Status.Added,Amount=20 },
+                new Product { Id = 5, CategoryId = 6, Name = "Cüzdan", Price = 400, Status = Status.Added,Amount=20 },
+                new Product { Id = 6, CategoryId = 3, Name = "Hoodie", Price = 400, Status = Status.Added,Amount=20 },
+                new Product { Id = 7, CategoryId = 7, Name = "Baf", Price = 400, Status = Status.Added,Amount=20 },
+                new Product { Id = 8, CategoryId = 4, Name = "Sweat", Price = 500,  Status = Status.Added,Amount=100 });
         }
     }
 }
