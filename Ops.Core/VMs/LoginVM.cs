5511e4cf192bcc4fcd46ed93d983e73bce3b6931
Beste Yasak <Beyasak@hotmail.com>
2023-11-20 13:14:18 +0300

    .

---
+++ Ops.Core/VMs/LoginVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ops.Core.VMs
+{
+    public class LoginVM
+    {
+        [EmailAddress(ErrorMessage = "Hatalı eposta.")]
+        [Required(ErrorMessage = "Lütfen E-mailinizi giriniz.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Lütfen kullanıcı adınızı giriniz.")]
+        [DataType(DataType.Password, ErrorMessage = "Parolanız gereken değerleri taşımıyor.")]
+        public string Password { get; set; }
+        public bool Remember { get; set; }
+    }
+}
---
+++ Ops.Web/Areas/Customer/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ops.Core.VMs;
+
+namespace Ops.Web.Areas.Customer.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginVM loginVM)
+        {
+            return View();
+        }
+    }
+}
