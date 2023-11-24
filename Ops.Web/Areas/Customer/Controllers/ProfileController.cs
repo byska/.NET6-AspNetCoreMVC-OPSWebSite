902ed32a18b9b10e35cc6f75e0cfd754fc73d234
Beste Yasak <Beyasak@hotmail.com>
2023-11-24 13:16:07 +0300

    .

---
+++ Ops.Web/Areas/Customer/Controllers/ProfileController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ops.Web.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
+
+    }
+}
